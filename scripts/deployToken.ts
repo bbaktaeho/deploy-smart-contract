161563ff7cd63d25b42c0372471474969f4fcfca
bbaktaeho <lth9036@gmail.com>
2021-11-01 22:26:53 +0900

    Update: file name

---
+++ scripts/deployToken.ts
@@ -0,0 +1,20 @@
+import hardhat from "hardhat";
+
+async function main() {
+  console.log("deploy start");
+
+  const AssetToken = await hardhat.ethers.getContractFactory("AssetToken");
+  const assetToken = await AssetToken.deploy(
+    "10000000000000000000000000", // totalSupply
+    "LeoToken", // Token name
+    "LEO" // Token symbol
+  );
+  console.log(`AssetToken address: ${assetToken.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
